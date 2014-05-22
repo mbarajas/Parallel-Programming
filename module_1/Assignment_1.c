7ebb8fed63b18fdb7e7d9653c3b0dcf847df2548
mbarajas <manuel.barajas@careerbuilder.com>
2014-05-22 10:51:18 -0400

    initial commit

---
+++ module_1/Assignment_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(int argc , char * argv[])
+{
+  int i,sum=0;
+
+  if(argc < 3)
+  {
+    printf("Not enough numbers to add up!");
+    exit(1);
+  }
+  
+  for(i=1;i<argc;i++)
+    sum = sum + atoi(argv[i]);
+
+  printf("%d",sum);
+
+}
